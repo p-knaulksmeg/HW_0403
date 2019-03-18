1bed88691af9040465592e50c5cee27cb7e997b7
Paul <pavelkeytof@gmail.com>
2019-03-18 22:55:50 +0300

    vk profile page created

--- app/src/main/java/by/paulk/hw_0403/MainActivity.java
+++ app/src/main/java/by/paulk/hw_0403/MainActivity.java
@@ -1,13 +1,19 @@
 package by.paulk.hw_0403;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Toolbar toolbar;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 }
